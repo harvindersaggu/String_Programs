e6f6498141a63f0cbf582bc061a2ff5929748f32
harvindersaggu <saggu.harvin@gmail.com>
2018-08-16 15:22:27 +0530

    first commit

---
+++ String_Programs/src/com/saggu/ReverseString.java
@@ -0,0 +1,27 @@
+package com.saggu;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the Sentence");
+		String input = scanner.nextLine();
+		char arr[] = input.toCharArray();
+		String output = "";
+		for (int i = arr.length - 1; i >= 0; i--) {
+			int k = i;
+			while (i >= 0 && arr[i] != ' ') {
+				i--;
+			}
+			int j = i + 1;
+			while (j <= k) {
+				output = output + arr[j];
+				j++;
+			}
+			output = output + " ";
+		}
+		System.out.println(output);
+	}
+
+}
